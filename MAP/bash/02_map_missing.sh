8b90ea6624288fba413e2ca817e3e52166e15bec
jtmiller <jtmiller@umn.edu>
2019-12-17 14:18:30 -0600

    sdf

---
+++ MAP/bash/02_map_missing.sh
@@ -0,0 +1,9 @@
+#!/bin/bash -l
+#SBATCH -t 48:00:00
+#SBATCH -p high
+#SBATCH --array=1-24
+#SBATCH --mem=20G
+
+script_dir='/home/jmiller1/QTL_agri/MAP'
+
+Rscript $script_dir/R/02_ELR_tspmap_with_missing.R --vanilla "$SLURM_ARRAY_TASK_ID" "${1}"
