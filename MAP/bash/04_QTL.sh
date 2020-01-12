65056c5f1dc3a0535d2150d9a1d950fe84568643
jtmiller <jtmiller@umn.edu>
2020-01-12 17:12:01 -0600

    dsf

--- MAP/bash/04_QTL.sh
+++ MAP/bash/04_QTL.sh
@@ -11,10 +11,10 @@ if [ "$SLURM_ARRAY_TASK_ID" == 1 ]; then
  Rscript $script_dir/R/04_final_bin_hk_QTL.R --vanilla "${1}"
 fi
 
-##if [ "$SLURM_ARRAY_TASK_ID" == 2 ]; then
-## Rscript $script_dir/R/04_final_bin_imp_QTL.R --vanilla "${1}"
-##fi
-##
-##if [ "$SLURM_ARRAY_TASK_ID" == 3 ]; then
-## Rscript $script_dir/R/04_final_norm_imp_QTL.R --vanilla "${1}"
-##fi
+if [ "$SLURM_ARRAY_TASK_ID" == 2 ]; then
+ Rscript $script_dir/R/04_final_bin_imp_QTL.R --vanilla "${1}"
+fi
+
+if [ "$SLURM_ARRAY_TASK_ID" == 3 ]; then
+ Rscript $script_dir/R/04_final_norm_imp_QTL.R --vanilla "${1}"
+fi
