c3cb83c1e03efb9776361b684b5fbd352c99506e
jtmiller <jtmiller@umn.edu>
2020-01-12 12:06:02 -0600

    sdf

---
+++ MAP/R/04_final_bin_hk_QTL.R
@@ -0,0 +1,66 @@
+#!/bin/R
+
+pop <- commandArgs(TRUE)[commandArgs(TRUE) %in% c('NBH','BRP','NEW','ELR')]
+library('qtl')
+source("/home/jmiller1/QTL_agri/MAP/control_file.R")
+mpath <- '/home/jmiller1/QTL_agri/data'
+fl <- paste0(pop,'.mapped.tsp.csv')
+fl <- file.path(mpath,fl)
+
+plotpub <- function(X) { png(paste0('~/public_html/',X,'.png')) }
+
+################################################################################
+################################################################################
+## Read cross
+cross <- read.cross(
+ file = fl,
+ format = "csv", genotypes=c("1","2","3"),
+ estimate.map = FALSE
+)
+
+cross <- sim.geno(cross,step=0,off.end=5, error.prob=0.025,map.function="kosambi")
+cross <- calc.genoprob(cross,step=1,error.prob=0.025,off.end=5)
+
+gg <- sim.geno(cross, step=1, error.prob=0.025, off.end=5, map.function="kosambi", n.draws=160)
+gg <- calc.genoprob(gg, step=1, error.prob=0.025, off.end=5, map.function="kosambi")
+gg_step2 <- reduce2grid(gg)
+################################################################################
+
+bin.add.hk <- stepwiseqtl(gg_step2, incl.markers=T, additive.only = T, model='binary', method = "hk", pheno.col = 4, scan.pairs = F, max.qtl=5)
+bin.add.hk.qtls <- summary(bin.add.hk)
+bin.add.hk.qtls <- makeqtl(gg_step2, chr=as.character(bin.add.hk.qtls$chr), pos=as.numeric(bin.add.hk.qtls$pos), what="draws")
+full.bin.hk <- stepwiseqtl(gg_step2, incl.markers=F, qtl=bin.add.hk.qtls, additive.only = F, model='binary', method = "hk", pheno.col = 4, scan.pairs = T, max.qtl=5, chr=c(1,2,5,8,13,18,24)
+
+################################################################################
+## binary
+scan.bin.hk <- scanone(cross, method = "hk", model = "binary", pheno.col = 4)
+scan.bin.em <- scanone(cross, method = "em", model = "binary", pheno.col = 4)
+scan.bin.mr <- scanone(cross, method = "mr", model = "binary", pheno.col = 4)
+bins <- data.frame(em=summary(scan.bin.em),mr=summary(scan.bin.mr))
+
+## normal
+scan.norm.mr <- scanone(cross, method = "mr", model = "normal", pheno.col = 5)
+scan.norm.imp <- scanone(cross, method = "imp", model = "normal", pheno.col = 5)
+norm.imp.qtl <- makeqtl(cross, chr=c(2,13,18), pos=c(30,33,28),  what="draws")
+################################################################################
+
+
+####################################################################################
+## PERMS WITH ALL LOCI
+perms.bin.em <- scanone(cross, method = "em", model = "binary", maxit = 1000,
+  n.perm = 10000, pheno.col = 4, n.cluster = 10)
+
+perms.norm.imp <- scanone(cross, method = "imp", model = "normal", maxit = 1000,
+  n.perm = 10000, pheno.col = 5, n.cluster = 10)
+####################################################################################
+#LOD thresholds (1000 permutations)
+#     lod
+#5%  4.20
+#10% 3.84
+# LOD thresholds (1000 permutations)
+#      lod
+# 5%  4.17
+# 10% 3.83
+################################################################################
+save.image(file.path(mpath,paste0(pop,'.rsave'))
+################################################################################
