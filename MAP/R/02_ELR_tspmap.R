a7461c9fdcb7f2dd2d4388399f67ad20bbee89ec
jtmiller <jtmiller@umn.edu>
2020-01-12 13:51:00 -0600

    sdf

--- MAP/R/02_ELR_tspmap.R
+++ MAP/R/02_ELR_tspmap.R
@@ -77,7 +77,7 @@ write.cross(cross,chr=i,filestem=filename,format="csv")
 Y <- c(0, as.numeric(gsub(".*:","",markernames(cross))))
 X <- 1:length(Y)
 
-png(paste0('~/public_html/',pop,'_RF_physpo_concord',i,'_tsp.png'))
+png(paste0('~/public_html/',pop,'_RF_physpo_concord',i,'_tsp.png'),width=1000,height=1000)
 par(mfrow=c(1,2))
  plotRF(cross)
  plot(c(1,length(X)),c(0,max(Y)),type="n", xlab=paste('chr',i), ylab='physical position')
