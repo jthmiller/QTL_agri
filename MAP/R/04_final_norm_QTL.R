14a511423f3a88913c7766766369fb300896a750
jtmiller <jtmiller@umn.edu>
2020-01-12 12:42:09 -0600

    dfg

--- MAP/R/04_final_norm_QTL.R
+++ MAP/R/04_final_norm_QTL.R
@@ -32,6 +32,8 @@ norm.add.qtls <- summary(norm.add)
 norm.add.qtls <- makeqtl(gg_step2, chr=as.character(norm.add.qtls$chr), pos=as.numeric(norm.add.qtls$pos), what="draws")
 full.norm.imp <- stepwiseqtl(gg_step2, incl.markers=F, qtl=norm.add.qtls, additive.only = F, model='normal', method = "imp", pheno.col = 5, scan.pairs = T, max.qtl=5, chr=c(1,2,5,8,13,18,24))
 
+grid.perms.norm.imp <- scanone(gg_step2, method = "imp", model = "normal", maxit = 1000, n.perm = 10000, pheno.col = 5, n.cluster = 10)
+
 ## normal
 scan.norm.mr <- scanone(cross, method = "mr", model = "normal", pheno.col = 5)
 scan.norm.imp <- scanone(cross, method = "imp", model = "normal", pheno.col = 5)
