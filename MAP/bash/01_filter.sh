92fef9cf3acb8b08d232e99ac55dced9cf0c4f62
jtmiller <jtmiller@umn.edu>
2020-01-17 15:12:35 -0600

    sdf

--- MAP/bash/01_filter.sh
+++ MAP/bash/01_filter.sh
@@ -2,7 +2,7 @@
 #SBATCH -t 48:00:00
 #SBATCH -p high
 #SBATCH --mem=60G
-#SBATCH  --output=/home/jmiller1/QTL_agri/MAP/bash/slurms/%x_%a_qtl.out
+#SBATCH  --output=/home/jmiller1/QTL_agri/MAP/bash/slurms/%x_%a_filter.out
 
 script_dir='/home/jmiller1/QTL_agri/MAP'
 
