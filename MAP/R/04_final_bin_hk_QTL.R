92ac943cec30415e5de6d699fc22b46e8f385b47
jtmiller <jtmiller@umn.edu>
2020-01-17 11:19:04 -0600

    sdf

--- MAP/R/04_final_bin_hk_QTL.R
+++ MAP/R/04_final_bin_hk_QTL.R
@@ -26,7 +26,7 @@ cross <- jittermap(cross, amount=1e-6)
 cross <- sim.geno(cross,step=0,off.end=5, error.prob=0.025,map.function="kosambi")
 cross <- calc.genoprob(cross,step=1,error.prob=0.025,off.end=5)
 
-gg_marks <- unlist(lapply(1:24,function(X) { pickMarkerSubset(pull.map(cross)[[X]], 0.50)} ))
+gg_marks <- unlist(lapply(1:24,function(X) { pickMarkerSubset(pull.map(cross)[[X]], 1)} ))
 if(pop == 'ELR.missing') gg_marks <- c(gg_marks,"AHR2a_del")
 
 gg <- pull.markers(cross,gg_marks)
