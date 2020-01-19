396ba73da606bfb9b1ca2f18fa0c6d6e47387b37
jtmiller <jtmiller@umn.edu>
2020-01-19 17:24:37 -0600

    sdf

--- MAP/bash/04_QTL.sh
+++ MAP/bash/04_QTL.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -l
 #SBATCH -t 48:00:00
 #SBATCH -p high
-#SBATCH --mem=30G
+#SBATCH --mem=60G
 #SBATCH --array=1-2
 #SBATCH  --output=/home/jmiller1/QTL_agri/MAP/bash/slurms/%x_%a.out
 
