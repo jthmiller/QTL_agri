cae5889103bfa86f971a394748e1dc4c6d887cf5
jtmiller <jtmiller@umn.edu>
2020-01-20 15:01:02 -0600

    sdf

--- MAP/R/permutations/04a_downsample.R
+++ MAP/R/permutations/04a_downsample.R
@@ -1,6 +1,6 @@
 #!/bin/R
-
 pop <- commandArgs(TRUE)[commandArgs(TRUE) %in% c('NBH','BRP','NEW','ELR','ELR.missing')]
+
 library('qtl')
 ##library('parallel')
 library('snow')
@@ -25,6 +25,11 @@ cross <- read.cross(
 )
 cross$pheno$pheno_norm <- round(nqrank(cross$pheno$Pheno),5)
 cross <- jittermap(cross)
+
+## Estimate gt prob and impute before downsample
+cross <- sim.geno(cross, stepwidth="fixed", step=1,off.end=5, error.prob=erp ,map.function="kosambi", n.draws=100)
+cross <- calc.genoprob(cross, stepwidth="fixed", step=1, error.prob=erp, off.end=5)
+
 dups <- findDupMarkers(cross, exact.only=F, adjacent.only=F)
 dups <- names(dups)
 if(pop == 'ELR.missing') dups <- c(dups,"AHR2a_del")
@@ -36,8 +41,7 @@ write.cross(cross,filestem=fl,format="csv")
 ################################################################################
 
 cross$pheno <- as.data.frame(cross$pheno)
-cross <- sim.geno(cross, stepwidth="fixed", step=1,off.end=5, error.prob=erp ,map.function="kosambi", n.draws=100)
-cross <- calc.genoprob(cross, stepwidth="fixed", step=1, error.prob=erp, off.end=5)
+
 cross <- reduce2grid(cross)
 
 ################################################################################
