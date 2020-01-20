9b8f508939402e30d1d30c6bd0fecc096bdaedad
jtmiller <jtmiller@umn.edu>
2020-01-20 15:02:56 -0600

    sdf

---
+++ MAP/bash/04b_bin_em_perms.sh
@@ -0,0 +1,9 @@
+#!/bin/bash -l
+#SBATCH -t 3:00:00
+#SBATCH -p high
+#SBATCH --mem=60G
+#SBATCH  --output=/home/jmiller1/QTL_agri/MAP/bash/slurms/%x_%a.out
+
+perms="$HOME/QTL_agri/MAP/R/permutations"
+
+Rscript $perms/04b_bin_em_perms.R --vanilla "${1}" "${2}"
