559e7299f9ae8e447c4f04a55fceee181a0acf29
jtmiller <jtmiller@umn.edu>
2020-01-21 12:05:52 -0600

    sdf

---
+++ MAP/R/permutations/04b_bin_hk_scan2.R
@@ -0,0 +1,38 @@
+#!/bin/R
+pop <- commandArgs(TRUE)[commandArgs(TRUE) %in% c('NBH','BRP','NEW','ELR','ELR.missing')]
+perm_count <- as.numeric(commandArgs(TRUE)[3])
+cores <- as.numeric(commandArgs(TRUE)[4])
+
+print(commandArgs(TRUE))
+print(paste(pop,perm_count))
+
+library('qtl')
+##library('parallel')
+library('snow')
+
+source("/home/jmiller1/QTL_agri/MAP/control_file.R")
+mpath <- '/home/jmiller1/QTL_agri/data'
+fl <- paste0(pop,'.mapped.tsp.csv')
+fl <- file.path(mpath,fl)
+
+################################################################################
+
+print(paste(cores,'cores'))
+erp <- 0.0025
+
+################################################################################
+
+################################################################################
+load(file.path(mpath,paste0(pop,'_downsampled.rsave')))
+################################################################################
+
+sex.phen <- pull.pheno(cross, "sex")
+names(cross$geno) <- ifelse(names(cross$geno) == "5","X",names(cross$geno))
+
+norm.imp.2 <- scantwo(cross, pheno.col=5, model="normal", method="imp",
+ incl.markers=F, chr = c(1:4,6:24),clean.output=T, clean.nmar=10, clean.distance=10,
+ assumeCondIndep=T, n.cluster=cores, intcovar=sex.phen)
+
+################################################################################
+save.image(file.path(mpath,paste0(pop,'_scan_norm_imp.rsave')))
+################################################################################
