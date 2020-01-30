65f008976e4e92065e79cf3329805ef8118aaf98
jtmiller <jtmiller@umn.edu>
2020-01-30 13:26:18 -0600

    cvx

--- MAP/bash/04c_bin_hk_scan2.sh
+++ MAP/bash/04c_bin_hk_scan2.sh
@@ -1,9 +1,10 @@
 #!/bin/bash -l
 #SBATCH -t 48:00:00
 #SBATCH -p high
-#SBATCH --mem=60G
-#SBATCH  --output=/home/jmiller1/QTL_agri/MAP/bash/slurms/scan2%x_%a.out
+#SBATCH --mem=10G
+#SBATCH --output=/home/jmiller1/QTL_agri/MAP/bash/slurms/scan2%x_%a.out
+#SBATCH --error=/home/jmiller1/QTL_agri/MAP/bash/slurms/error/perm%x_%a.error
 
 perms="$HOME/QTL_agri/MAP/R/permutations"
 
-Rscript $perms/04c_bin_hk_scan2.R --vanilla "${1}" "${2}"
+Rscript $perms/04c_bin_hk_scan2.R --vanilla "${1}" "${2}" "${3}"
