e7bea32bdbfbb5d1132a74421092ac967f2419f9
jtmiller <jtmiller@umn.edu>
2020-01-30 20:44:30 -0600

    sdf

--- MAP/bash/04b_bin_hk_perms.sh
+++ MAP/bash/04b_bin_hk_perms.sh
@@ -3,7 +3,11 @@
 #SBATCH -p high
 #SBATCH --mem=60G
 #SBATCH  --output=/home/jmiller1/QTL_agri/MAP/bash/slurms/perm%x_%a.out
+#SBATCH --output=/home/jmiller1/QTL_agri/MAP/bash/slurms/perm%x_%a.out
 
 perms="$HOME/QTL_agri/MAP/R/permutations"
 
-Rscript $perms/04b_bin_hk_perms.R --vanilla "${1}" "${2}" "${3}"
+## 04b_bin_em_perms.sh --vanilla pop perm_count cores arraynum
+# sbatch -J "NBH_PBE" -p high -t 48:00:00 $bashsc/04b_bin_em_perms.sh "--vanilla" 'NBH' 12 1
+
+Rscript $perms/04b_bin_hk_perms.R "${1}" "${2}" "${3}" "${4}" "${SLURM_ARRAY_TASK_ID}"
