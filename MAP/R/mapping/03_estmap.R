98bbac8f73cf88e98445c9df8f12adc75112acdd
jtmiller <jtmiller@umn.edu>
2020-02-17 17:36:02 -0600

    sdf

--- MAP/R/mapping/03_estmap.R
+++ MAP/R/mapping/03_estmap.R
@@ -5,7 +5,10 @@ pop <- commandArgs(TRUE)[commandArgs(TRUE) %in% c('NBH','BRP','NEW','ELR')]
 
 ##source("/home/jmiller1/QTL_agri/MAP/control_file.R")
 mpath <- '/home/jmiller1/QTL_agri/data'
-mapfile <- paste0(pop,'_unmapped_all_mark_imputed_',i,'_tsp.csv')
+#mapfile <- paste0(pop,'_unmapped_all_mark_imputed_',i,'_tsp.csv')
+#filename <- file.path(mpath,mapfile)
+
+mapfile <- paste0(pop,'_imputed_',i,'_tsp.csv')
 filename <- file.path(mpath,mapfile)
 
 #libs2load<-c('devtools','qtl',"ASMap","qtlTools","TSP","TSPmap")
