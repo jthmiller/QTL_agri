3fd408c67f4c7bed4ea82448f7f3681d3670836a
jtmiller <jtmiller@umn.edu>
2020-02-21 11:55:29 -0600

    sdf

--- MAP/bash/04_bin_mr_scan2.sh
+++ MAP/bash/04_bin_mr_scan2.sh
@@ -2,7 +2,8 @@
 #SBATCH -t 48:00:00
 #SBATCH -p high
 #SBATCH --mem=60G
-#SBATCH  --output=/home/jmiller1/QTL_agri/MAP/bash/slurms/scan2%x_%a.out
+#SBATCH  --output=/home/jmiller1/QTL_agri/MAP/bash/slurms/mr_scan2_%x_%a.out
+#SBATCH  --error=/home/jmiller1/QTL_agri/MAP/bash/slurms/mr_scan2_%x_%a.err
 
 perms="$HOME/QTL_agri/MAP/R/final"
 
