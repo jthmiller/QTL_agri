590c859e04808f54b98c4a9ff977020d36ebdeed
jtmiller <jtmiller@umn.edu>
2020-02-24 01:29:51 -0600

    sdf

--- MAP/bash/00_master.sh
+++ MAP/bash/00_master.sh
@@ -28,6 +28,11 @@ bashsc="$HOME/QTL_agri/MAP/bash"
 sbatch -J "MRB.NBH" -p high -t 48:00:00 $bashsc/04_bin_mr_scan2.sh 'NBH' 22
 #################################################################################
 
+### TWO LOCUS SCAN $1=population $2=number of cores to use
+bashsc="$HOME/QTL_agri/MAP/bash"
+sbatch -J "NIMP.NBH" -p high -t 48:00:00 $bashsc/04_norm_imp_scan2.sh 'NBH' 22
+#################################################################################
+
 ### SCAN2 PERMS
 bashsc="$HOME/QTL_agri/MAP/bash"
 
