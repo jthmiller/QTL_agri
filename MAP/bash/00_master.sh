6e27738c52b6b702aa739287dfc43c181ea0c0af
jtmiller <jtmiller@umn.edu>
2020-02-25 15:40:13 -0600

    asd

--- MAP/bash/00_master.sh
+++ MAP/bash/00_master.sh
@@ -30,9 +30,9 @@ sbatch -J "ELR" $bashsc/03_write_map_cross.sh 'ELR'
 bashsc="$HOME/QTL_agri/MAP/bash"
 
 args=( '--vanilla' 'NBH' '1' '1' 'binary' )
-varem=$(sbatch --mem=10G -p low --array=1-100 -t 3:00:00 -J "PERM.EM.NBH" $bashsc/05_perms.sh "${args[@]}" 'em' | cut -f4 -d' ')
-varhk=$(sbatch --mem=10G -p low --array=1-2 -t 3:00:00 -J "PERM.HK.NBH" $bashsc/05_perms.sh "${args[@]}" 'hk' | cut -f4 -d' ')
-varmr=$(sbatch --mem=10G -p low --array=1-2 -t 3:00:00 -J "PERM.MR.NBH" $bashsc/05_perms.sh "${args[@]}" 'mr' | cut -f4 -d' ')
+varem=$(sbatch --mem=10G -p low --array=1-50 -t 6:00:00 -J "PERM.EM.NBH" $bashsc/05_perms.sh "${args[@]}" 'em' | cut -f4 -d' ')
+varhk=$(sbatch --mem=10G -p low --array=1-50 -t 6:00:00 -J "PERM.HK.NBH" $bashsc/05_perms.sh "${args[@]}" 'hk' | cut -f4 -d' ')
+varmr=$(sbatch --mem=10G -p low --array=1-50 -t 6:00:00 -J "PERM.MR.NBH" $bashsc/05_perms.sh "${args[@]}" 'mr' | cut -f4 -d' ')
 
 args=( '--vanilla' 'NBH' '1' '1' 'normal' 'imp' )
 varim=$(sbatch --mem=10G -p high --array=1-2 -t 3:00:00 -J "PERM.IMP.NBH" $bashsc/05_perms.sh "${args[@]}" | cut -f4 -d' ')
